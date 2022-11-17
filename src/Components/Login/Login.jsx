92cf71e71067cda97c0791b814e68776c607fcb2
Ville Varjus <e1900513@edu.vamk.fi>
2022-11-17 13:07:05 +0200

    added Login component with styles

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import './login.css'
+
+const Login = () => {
+  return (
+    <div className='login__container'>
+
+        <h1 className="login__title">
+            Login
+        </h1>
+
+        <div className="login__section">
+
+            <form className="login__form">
+                <input type="text" id='username' className="login__username" placeholder='Username'/>
+                <div className="line"></div>
+                 <br></br>
+                <input type="password" id='password' className="login__password" placeholder='Password'/>
+                <div className="line"></div>
+                <button className="login">Sign in</button>
+            </form>
+
+            <p className="description">
+                    Or login with
+            </p>
+
+        <div className="social__container">
+
+            <ul class="socials">
+                <li class="icon facebook">
+                    <span class="tooltip">Facebook</span>
+                    <span><i class="uil uil-facebook-f"></i></span>
+                </li>
+                <li class="icon twitter">
+                    <span class="tooltip">Twitter</span>
+                    <span><i class="uil uil-twitter"></i></span>
+                </li>
+                <li class="icon instagram">
+                    <span class="tooltip">Instagram</span>
+                    <span><i class="uil uil-instagram"></i></span>
+                </li>
+                <li class="icon github">
+                    <span class="tooltip">Github</span>
+                    <span><i class="uil uil-github"></i></span>
+                </li>
+            </ul>
+            </div>
+
+            <div className="register__section">
+            Not a member?
+            <a href='/register' className="register">
+                    Register here
+                </a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
